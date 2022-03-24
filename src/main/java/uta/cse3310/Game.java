7db9e8e6f645175341349e441f19308526ae19a5
Lys <alyssahammonds5453@gmail.com>
2022-03-24 18:10:36 -0500

    test

--- src/main/java/uta/cse3310/Game.java
+++ src/main/java/uta/cse3310/Game.java
@@ -56,17 +56,14 @@ public class Game {
     // most likely a player per thread
     // where each thread get the UserEvents
     // returns the winner
-    public void play_game()
-    {
+    public void play_game() {
         // deal cards for each player
         // and remove the from the deck
         // to prevent duplicates
         // cards will be added back to deck when
         // shuffling or drawing
-        for(Player p: players)
-        {
-            for(int i = 0; i < p.Cards.length; i++)
-            {
+        for (Player p : players) {
+            for (int i = 0; i < p.Cards.length; i++) {
                 p.Cards[i] = deck.get(0);
                 deck.remove(0);
             }
@@ -74,46 +71,35 @@ public class Game {
 
         // 1st betting round
 
-
-
         // Draw round
         // note: this is not tested
 
         // max of 3 cards exchanged
         // VARIABLE IN UserEvent TO KEEP TRACK
         // this starts with player zero in the list
-        for( turn = 0; turn < players.size(); turn++)
-        {
+        for (turn = 0; turn < players.size(); turn++) {
             // only legal actions here are DRAW or STAND
             // get user event and check if it was from the player.
             UserEvent event = new UserEvent();
-            while(true)
+            while (true) // infinite loop?
             {
                 // get the event HERE
                 // add code to get it
-                if(event.playerID == turn && (event.event != UserEventType.DRAW
-                    || event.event != UserEventType.STAND))
-                {
-                    if(event.event == UserEventType.STAND)
-                    {
+                if (event.playerID == turn && (event.event != UserEventType.DRAW
+                        || event.event != UserEventType.STAND)) {
+                    if (event.event == UserEventType.STAND) {
                         // this will end turn and proceed to next player
                         break;
-                    }
-                    else
-                    {
+                    } else {
                         // iterate for the amount to draw specified
                         // from the event and based on the indeces
                         // that is in the event
-                        for(int i = 0; i < event.amount_to_draw; i++)
-                        {
-                            try
-                            {
+                        for (int i = 0; i < event.amount_to_draw; i++) {
+                            try {
                                 // add current card at given index to end of deck
                                 deck.add(players.get(turn).Cards[event.give_card_indexes[i]]);
                                 players.get(turn).Cards[event.give_card_indexes[i]] = get_card(deck);
-                            }
-                            catch (ArrayIndexOutOfBoundsException e)
-                            {
+                            } catch (ArrayIndexOutOfBoundsException e) {
                                 // debug
                                 System.out.println("DRAW Card error: -1 in index");
                             }
@@ -125,19 +111,14 @@ public class Game {
             }
         }
 
-
         // 2nd betting round
 
-
         // showdown
-
-
 
     }
 
     // gets a card from the front of passed in deck
-    private Card get_card(ArrayList<Card> deck)
-    {
+    private Card get_card(ArrayList<Card> deck) {
         Card card = deck.get(0);
         deck.remove(0);
         return card;
@@ -146,56 +127,47 @@ public class Game {
     // remove all player's Cards
     // and randomize the order in the shuffle_deck
     // will be 52 cards in size
-    private void shuffle_deck(ArrayList<Card> deck)
-    {
+    private void shuffle_deck(ArrayList<Card> deck) {
         // remove player's cards for cases
         // when game is replayed
-        for(Player p: players)
-        {
-            for(int i = 0; i < p.Cards.length; i++)
-            {
+        for (Player p : players) {
+            for (int i = 0; i < p.Cards.length; i++) {
                 // add card back to deck
                 deck.add(p.Cards[i]);
                 // remove card from hand
-                //p.Cards[i] = NULL;
+                // p.Cards[i] = NULL;
             }
         }
 
         // shuffle current deck
         Collections.shuffle(deck);
-        if(deck.size() != 52)
-        {
+        if (deck.size() != 52) {
             System.out.println("Error in deck shuffle, not 52 cards in deck.");
         }
 
         // print cards
         // debug
         System.out.println();
-        for(Card card: deck)
-        {
-            System.out.println(card.suite.toString() +" "+card.value.toString());
+        for (Card card : deck) {
+            System.out.println(card.suite.toString() + " " + card.value.toString());
         }
 
     }
 
     // this adds all 52 cards to the deck in order
     // will need to shuffle in game
-    private void create_deck(ArrayList<Card> deck)
-    {
+    private void create_deck(ArrayList<Card> deck) {
         // for each suite and each value add the card
-        for(Card.Suite suite: Card.Suite.values())
-        {
-            for(Card.Value value: Card.Value.values())
-            {
+        for (Card.Suite suite : Card.Suite.values()) {
+            for (Card.Value value : Card.Value.values()) {
                 Card card = new Card();
                 card.suite = suite;
                 card.value = value;
                 deck.add(card);
                 // print all cards for error checking
-                //System.out.println(card.suite.toString() +" "+card.value.toString());
+                // System.out.println(card.suite.toString() +" "+card.value.toString());
             }
         }
     }
-
 
 }
